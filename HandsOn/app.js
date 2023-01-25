3ecf974ee038c2abd20bce64661b4b56626137d2
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-01-24 20:19:03 -0700

    6 video

--- HandsOn/app.js
+++ HandsOn/app.js
@@ -1,1 +1,5 @@
-require('./count')
+var stuff = require('./stuff');
+
+console.log(stuff.counter(['1','2','3']));
+console.log(stuff.adder(1,3));
+console.log(stuff.pi);
---
+++ HandsOn/stuff.js
@@ -0,0 +1,9 @@
+module.exports.counter=function(arr){
+    return 'There are '+arr.length+'elements in the array';
+};
+
+module.exports.adder= function(a,b){
+    return `the sum of 2 numbers is ${a+b}`;
+};
+module.exports.pi=3.14;
+
